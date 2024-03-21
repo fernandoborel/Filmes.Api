0caa13e4ccaa056a839cf562fae6ac29a3311cc7
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-03-21 16:14:23 -0300

    feat. adicionando controller e model

---
+++ src/Filmes.Domain/Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace Filmes.Domain.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Duracao { get; set; }
+}
