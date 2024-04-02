a70f92fac4acffa00a414757d49bc115082a7b82
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-04-02 16:35:20 -0300

    feat. CinemasController adicionado.

---
+++ src/Filmes.Application/Dtos/AtualizarCinemaDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filmes.Application.Dtos;
+
+public class AtualizarCinemaDto
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [Length(3,100, ErrorMessage = "Nome deve conter entre {0} e {1} caracteres.")]
+    public string Nome { get; set; }
+}
