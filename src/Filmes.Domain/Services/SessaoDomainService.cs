73e5776f383e79ef8dfad0d279efae578867e1ac
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2024-04-06 18:30:46 -0300

    feat. adicionando sessão

---
+++ src/Filmes.Domain/Services/SessaoDomainService.cs
@@ -0,0 +1,66 @@
+﻿using Filmes.Domain.Interfaces.Repositories;
+using Filmes.Domain.Interfaces.Services;
+using Filmes.Domain.Models;
+
+namespace Filmes.Domain.Services;
+
+public class SessaoDomainService : ISessaoDomainService
+{
+    private readonly ISessaoRepository _sessaoRepository;
+
+    public SessaoDomainService(ISessaoRepository sessaoRepository)
+    {
+        _sessaoRepository = sessaoRepository;
+    }
+
+    public async Task Adicionar(Sessao sessao)
+    {
+        await _sessaoRepository.AddAsync(sessao);
+    }
+
+    public async Task Atualizar(Sessao sessao)
+    {
+        var ses = await _sessaoRepository.GetByIdAsync(sessao.Id);
+        if (ses != null)
+        {
+            ses.FilmeId = sessao.FilmeId;
+            ses.CinemaId = sessao.CinemaId;
+            ses.Filme = sessao.Filme;
+            ses.Cinema = sessao.Cinema;
+            ses.Horario = sessao.Horario;
+
+            await _sessaoRepository.UpdateAsync(ses);
+        }
+    }
+
+    public async Task Remover(Sessao sessao)
+    {
+        var ses = await _sessaoRepository.GetByIdAsync(sessao.Id);
+        if (ses != null)
+        {
+            await _sessaoRepository.DeleteAsync(ses);
+        }
+    }
+
+    public void Dispose()
+    {
+        _sessaoRepository?.Dispose();
+    }
+
+    public Task<Sessao> ObterPorId(int id)
+    {
+        var ses = _sessaoRepository.GetByIdAsync(id);
+        return ses;
+    }
+
+    public async Task<IEnumerable<Sessao>> ObterSessao(string sessao)
+    {
+        return await _sessaoRepository.GetBySessaoAsync(sessao);
+    }
+
+    public async Task<IEnumerable<Sessao>> ObterTodos()
+    {
+        var sessoes = await _sessaoRepository.GetAllAsync();
+        return sessoes;
+    }    
+}
---
+++ src/Filmes.Infra.Data/Repositories/SessaoRepository.cs
@@ -0,0 +1,25 @@
+﻿using Filmes.Domain.Interfaces.Repositories;
+using Filmes.Domain.Models;
+using Filmes.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmes.Infra.Data.Repositories;
+
+public class SessaoRepository : BaseRepository<Sessao>, ISessaoRepository
+{
+    private readonly SqlServerContext _context;
+
+    public SessaoRepository(SqlServerContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Sessao>> GetBySessaoAsync(string sessao)
+    {
+        return await _context.Sessoes
+                             .Include(s => s.Cinema)
+                             .Include(s => s.Filme)
+                             .Where(s => s.Horario.ToString().Contains(sessao))
+                             .ToListAsync();
+    }
+}
